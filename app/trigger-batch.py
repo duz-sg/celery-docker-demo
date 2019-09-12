b80bcba6c4c4e14cb462b22fb1f1816f134b7a27
Du Zheng <du.zheng@dtone.com>
2019-09-12 15:30:15 +0800

    Starting with 10 tasks

--- app/trigger-batch.py
+++ app/trigger-batch.py
@@ -2,6 +2,6 @@ from celery.utils.log import get_task_logger
 from worker import app
 
 
-for i in range(1000):
+for i in range(10):
     app.send_task("batch", args=[i])
     print(f"Sent task {i}")
